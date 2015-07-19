6cd84dab8bca1b6ab92ed590259c24d08373d2dd
Karolina <ka.rlas@yahoo.com>
2015-07-19 20:34:12 +0000

    Added my name

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "YOUR_NAME_HERE";
+	var myName = "Karolina";
 	app.get("/", function (req, resp) {
 		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
+		resp.end(cle);
 	});
 
 })
